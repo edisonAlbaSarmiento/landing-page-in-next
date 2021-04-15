49d0130bc25c9f65ca93f175e814a6bc2d0bb816
edison <edi9708@hotmail.com>
2021-04-15 18:24:38 -0500

    add new atoms

---
+++ src/components/atoms/header/index.js
@@ -0,0 +1,7 @@
+import { Content } from './styles';
+
+function Header() {
+  return <Content>{children}</Content>;
+}
+
+export default Header;
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import Link from 'next/link';
+
 function About() {
   return (
     <div>
