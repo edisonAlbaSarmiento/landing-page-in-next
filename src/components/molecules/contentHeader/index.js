0a1d28d586611cd366d34ffb4c3178947cedce55
edison <edi9708@hotmail.com>
2021-04-19 17:07:12 -0500

    update in styles of header

--- src/components/molecules/contentHeader/index.js
+++ src/components/molecules/contentHeader/index.js
@@ -1,17 +1,56 @@
-import { Content } from './styles';
+import {
+  Content,
+  ContentTitle,
+  ContentImage,
+  ContentImageBackBackground,
+  ContentImageBackground,
+  Title,
+  SubTitle,
+} from './styles';
 import Image from 'next/image';
+import { Typography } from 'components/atoms';
 
 function ContentHeader(props) {
   return (
-    <Content>
-      <h1>hola</h1>
+    <>
       <Image
-        src="https://raw.githubusercontent.com/edisonAlbaSarmiento/landing-page-in-next/main/src/static/images/Group10.jpeg"
+        src="https://raw.githubusercontent.com/edisonAlbaSarmiento/landing-page-in-next/main/src/static/images/iconImaginamos.png"
         alt="Picture of the author"
-        width={500}
-        height={500}
+        width={190}
+        height={80}
       />
-    </Content>
+      <Content>
+        <ContentTitle>
+          <Typography variant="header">
+            <Title>Vivimos para transformar el futuro</Title>
+          </Typography>
+          <Typography variant="title">
+            <SubTitle>
+              Construyendo ecosistemas digitales de alto impacto.
+            </SubTitle>
+          </Typography>
+        </ContentTitle>
+
+        <ContentImageBackBackground>
+          <ContentImageBackground>
+            <Image
+              src="https://raw.githubusercontent.com/edisonAlbaSarmiento/landing-page-in-next/main/src/static/images/BackgroundHeader.png"
+              alt="Picture of the author"
+              width={500}
+              height={500}
+            />
+          </ContentImageBackground>
+          <ContentImage>
+            <Image
+              src="https://raw.githubusercontent.com/edisonAlbaSarmiento/landing-page-in-next/main/src/static/images/BackgroundBack.png"
+              alt="Picture of the author"
+              width={500}
+              height={500}
+            />
+          </ContentImage>
+        </ContentImageBackBackground>
+      </Content>
+    </>
   );
 }
 
