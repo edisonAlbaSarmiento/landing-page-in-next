68a90cb40aac484c48fdabf7abb77d30db62636b
edison <edi9708@hotmail.com>
2021-04-23 15:44:58 -0500

    delete styles

--- src/pages/about-us/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Body = styled.div`
-  background-color: ${(props) => props.theme.color.bluetest};
-`;
-
-export const Title = styled.h1`
-  color: ${(props) => props.theme.color.redtest};
-`;
