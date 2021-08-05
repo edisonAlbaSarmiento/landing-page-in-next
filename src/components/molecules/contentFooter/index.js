823c97e1331569170a5f276a6cc632c07234be3a
edison <edi9708@hotmail.com>
2021-08-05 00:38:48 -0500

    add footer

--- src/components/molecules/contentFooter/index.js
+++ src/components/molecules/contentFooter/index.js
@@ -1,13 +1,35 @@
-import { Content, ImageCard } from './styles';
+import {
+  Content,
+  ImageCard,
+  ContentPrimary,
+  ContentPrimaryFooter,
+  ContentChild,
+  ContentNetworks,
+} from './styles';
 
 function ContentFooter(params) {
   return (
     <Content>
-      <div style={{ backgroundColor: 'yellow' }}>
+      <ContentPrimary>
         <ImageCard src='https://i0.wp.com/www.imaginamos.com/wp-content/uploads/2021/04/Vector.png?fit=208%2C208&ssl=1' />
-      </div>
-      <div style={{ backgroundColor: 'yellow' }}>Edison footer </div>
-      <div style={{ backgroundColor: 'yellow' }}>Edison footer </div>
+        <ContentPrimaryFooter>
+          <ContentChild>Términos y condiciones</ContentChild>
+          <ContentChild>Políticas </ContentChild>
+          <ContentChild>Contacto </ContentChild>
+          <ContentChild>
+            En Imaginamos nos encanta conocer todas sus opiniones, sugerencias o
+            reconocimientos sobre nuestros servicios. Para presentar su
+            manifestación en la modalidad de Petición, Queja, Reclamo,
+            Sugerencia y/o Felicitación, haga clic aquí
+          </ContentChild>
+        </ContentPrimaryFooter>
+      </ContentPrimary>
+      <ContentNetworks>
+        <div style={{ backgroundColor: 'yellow' }}>R </div>
+        <div style={{ backgroundColor: 'yellow' }}>R </div>
+        <div style={{ backgroundColor: 'yellow' }}>R </div>
+        <div style={{ backgroundColor: 'yellow' }}>R </div>
+      </ContentNetworks>
     </Content>
   );
 }
--- src/components/molecules/contentFooter/styles.js
+++ src/components/molecules/contentFooter/styles.js
@@ -7,10 +7,66 @@ export const Content = styled.div`
   align-content: center;
   justify-content: center;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-around;
+  @media (max-width: 768px) {
+    width: fit-content;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    padding: 2%;
+  }
+`;
+
+export const ContentPrimary = styled.div`
+  display: flex;
+  width: 30%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  @media (max-width: 768px) {
+    width: fit-content;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const ContentPrimaryFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+export const ContentChild = styled.div`
+  padding: 0% 0% 3% 6%;
+
+  @media (max-width: 768px) {
+    padding: 2%;
+  }
 `;
 
 export const ImageCard = styled.img`
   width: 100px;
   height: 100px;
+  @media (max-width: 768px) {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+export const ContentNetworks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+  width: 22%;
+  @media (max-width: 768px) {
+    width: 48%;
+    padding: 5%;
+  }
 `;
